23043a43cc3d900d46869c1d45f47575499a071d
Jakub Pietrala <j4ckpie@users.noreply.github.com>
2025-03-18 13:40:26 +0100

    Added input flags handling

--- src/main.c
+++ src/main.c
@@ -1,6 +1,32 @@
 #include <stdio.h>
 
+#include <getopt.h>
+#include "flags_config.h"
+
 int main(int argc, char **argv) {
-    printf("Hello World!\n");
+    int opt;
+    int option_index;
+
+    // Handling input flags
+    while((opt = getopt_long(argc, argv, "hkxfi:", long_options, &option_index)) != -1) {
+        switch(opt) {
+            case 'h':
+                // Display help
+                break;
+            case 'k':
+                // Set the amount of subgraphs
+                break;
+            case 'x':
+                // Set max percent margin
+                break;
+            case 'f':
+                // Set filetype
+                break;
+            case 'i':
+                // Set path to input file
+                break;
+        }
+    }
+
     return 0;
 }
